c7335845e64b782c6ca798cac2d0e175a71b8206
pashynskyi <59894423+pashynskyi@users.noreply.github.com>
2020-07-25 20:35:00 +0300

    Created header

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './assets/images/logo.svg';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <img src={logo} alt='logo' />
+        <nav>
+          <ul>
+            <li><a href="">About me</a></li>
+            <li><a href="">Relationships</a></li>
+            <li><a href="">Requirements</a></li>
+            <li><a href="">Users</a></li>
+            <li><a href="">Sign Up</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
