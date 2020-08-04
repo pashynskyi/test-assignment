0856534a84c9b653cdeae8eef154a8d34d22bdce
pashynskyi <59894423+pashynskyi@users.noreply.github.com>
2020-08-04 14:47:45 +0300

    Created menu-burger

--- src/Header.js
+++ src/Header.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './assets/images/logo.svg';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <header className="header">
-      <div className="header-content">
-        <NavLink to='/'>
-          <img src={logo} alt='logo' />
-        </NavLink>
-        <nav>
-          <ul>
-            <li><NavLink to="#">About me</NavLink></li>
-            <li><NavLink to="#">Relationships</NavLink></li>
-            <li><NavLink to="#">Requirements</NavLink></li>
-            <li><NavLink to="#">Users</NavLink></li>
-            <li><a href="#register">Sign Up</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  )
+class Header extends React.Component {
+  state = {
+    activeClass: false //header-burger
+  }
+
+  toggleActiveClass = () => {
+    this.setState({ activeClass: !this.state.activeClass })
+  }
+
+  render() {
+    return (
+      <header className="header">
+        <div className="header-content">
+          <NavLink to='/'>
+            <img src={logo} alt='logo' />
+          </NavLink>
+          <div
+            className={!this.state.activeClass ? "header-burger" : "header-burger active"}
+            onClick={this.toggleActiveClass}>
+            <span></span>
+          </div>
+          <nav className={!this.state.activeClass ? "header-menu" : "header-menu active"}>
+            <ul>
+              <li><NavLink to="#">About me</NavLink></li>
+              <li><NavLink to="#">Relationships</NavLink></li>
+              <li><NavLink to="#">Requirements</NavLink></li>
+              <li><NavLink to="#">Users</NavLink></li>
+              <li><a href="#register">Sign Up</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    )
+  }
 }
 
 export default Header;
